69426fb9a1adc4e798291ee59447f8b76a97c3fc
lenafoon <lena.foon@gmail.com>
2023-07-08 16:51:09 +0200

    Iteration 8

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,60 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0,
+    this.intervalId = null;
+  
+    this.getMinutes() 
+      return Math.floor(this.currentTime / 60)
+    
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const stringValue = value.toString();
+    if (stringValue.length < 2) {
+      return "0".concat(stringValue);
+    } else {
+      return stringValue;
+    }
   }
+  
 
   stop() {
-    // ... your code goes here
+    
+      clearInterval(this.intervalId);
+    
   }
 
   reset() {
-    // ... your code goes here
-  }
+      this.currentTime = 0;
+    }
+  
 
   split() {
-    // ... your code goes here
+      const minutes = this.computeTwoDigitNumber(this.getMinutes());
+      const seconds = this.computeTwoDigitNumber(this.getSeconds());
+      return `${minutes}:${seconds}`;
+
+    }
+   
   }
-}
+  
+
